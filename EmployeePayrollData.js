ebb7effed74633d1bdb60a001219583b6acddf96
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-11-18 00:32:15 +0530

    [Praveen] Add: Added more data attributes and converted the date time string to only needed data.

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -5,18 +5,26 @@ class EmployeePayrollData
     id; 
     name;
     salary;
+    /// UC13 -- Adding the gender and start date to employee payroll class
+    gender;
+    startDate;
     /// Parameterised constructor to initialise the data instances with value passed
-    constructor(id, name, salary)
+    constructor(...params)
     {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
     /// Using a member instance to display the stored details of the employees
     /// Returning a string representation of employees data
     toString()
     {
-        return `\n ID : ${this.id} , Name : ${this.name} , Salary : ${this.salary}`;
+        /// UC13 -- Adding the date in short representation
+        const options = { year: 'numeric', month: 'long', day: 'numeric'};
+        const emplpyeeStartDate = this.startDate == undefined ? "Undefined" : this.startDate.toLocaleDateString("en-US", options);
+        return `\n ID : ${this.id} , Name : ${this.name} , Salary : ${this.salary}, Gender = ${this.gender}, Start Date : ${emplpyeeStartDate}`;
     }
 }
 /// Creating an instance of the class with data instance values
@@ -27,3 +35,7 @@ console.log(employeesPayrollData.toString());
 employeesPayrollData.name = "Gopi";
 /// Printing the details of the object using the toString method after reinitialising the instance member
 console.log(employeesPayrollData.toString());
+/// Creating an instance of the class with data instance values
+let otherEmployeesPayrollData = new EmployeePayrollData(1, "Terissa", 40000, "F", new Date());
+/// Printing the details of the object using the toString method
+console.log(otherEmployeesPayrollData.toString());
